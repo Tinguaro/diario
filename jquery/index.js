5c8a7a50183be9ea3d378362f0d47a2a7b00aec5
Tinguaro Cubas Saiz <tinguarocubas@gmail.com>
2015-05-28 17:02:32 +0100

    puesto servicio

--- jquery/index.js
+++ jquery/index.js
@@ -5,27 +5,41 @@ $(function(){
 function insertar() {
 	$("#insertar").css("display", "block");
 	$("#entradas").css("display", "none");
+	$("#entradasViejas").css("display", "none");
 }
 
 function entradas() {
 	$("#insertar").css("display", "none");
-	$("#entradas").css("display", "block");	
+	$("#entradas").css("display", "block");
+	$("#entradasViejas").css("display", "block");	
 }
 
-function referenciar() {
+function submit() {
+	document.forms["formulario"].submit();
 }
 
 function leer() {
 	$.ajax({
-	  method: "GET",
-	  url: "https://diariotfg.herokuapp.com/entradas",
-	  success: exito,
-	  error: noexito,
+		type: "GET",
+		url: "https://diariotfg.herokuapp.com/entradas",
+		crossDomain: true,
+		success: exito,
+		error: noexito
 	});
 }
 
 function exito(data) {
 	console.log(data);
+	var valores = data.split(';');
+	var i = 0;
+	var text = "";
+	while(i < valores.length) {
+		if(valores[i].length != 0) {
+			text = "<div class='panel panel-primary'><div class='panel-heading'><h4>"+ valores[i] +"<h4></div><div class='panel-body'>"+ valores[i + 1] +"</div></div>" + text;
+		}
+		i += 2;
+	}
+	$("#entradas").append(text);
 }
 
 function noexito(error) {
